de17146f492262f72d8dbbb4e51ec8ffc3b9c0e8
wangqiran <1771524717@qq.com>
2026-05-15 18:41:37 +0800

    Update DBConnection.java

--- java_code/DBConnection.java
+++ java_code/DBConnection.java
@@ -12,7 +12,7 @@ public class DBConnection {
     private Connection connection;
 
     // Database configuration
-    private static final String DB_URL = "the real pass of your asl file";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/group2_gym_system";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "you mysql password here"; // Change this to your MySQL password
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
