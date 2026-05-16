12af34ef9b094bb533193ef0ae1435933072d649
wangqiran <1771524717@qq.com>
2026-05-16 15:43:41 +0800

    Update DBConnection.java

--- java/DBConnection.java
+++ java/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection {
     // Database configuration
     private static final String DB_URL = "jdbc:mysql://localhost:3306/group2_gym_system";
     private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "Chy070204"; // Change this to your MySQL password
+    private static final String DB_PASSWORD = "your password for mySQL is here"; // Change this to your MySQL password
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     /**
